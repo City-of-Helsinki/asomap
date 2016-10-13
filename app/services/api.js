a720d862026b929bfb3c0c95b40d4bc0b35e3ab7
Oskari Hiltunen <felix.loether@gmail.com>
2016-10-13 15:39:20 +0300

    Add services/api.getUnits, refs #7

--- app/services/api.js
+++ app/services/api.js
@@ -8,4 +8,16 @@ export function get(url, queryParams) {
   return window.fetch(fullUrl).then(response => response.json());
 }
 
-export default {};
+function getUnits() {
+  return get(
+    'unit/',
+    {
+      service: '25304',
+      only: 'name,location,street_address,address_zip,municipality,www_url',
+      page: '1',
+      page_size: '1000',
+    }
+  ).then(data => data.results);
+}
+
+export default { getUnits };
