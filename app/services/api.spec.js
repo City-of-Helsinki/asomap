d652117a9c6d236f68dfabd70a029dafb640e96f
Oskari Hiltunen <felix.loether@gmail.com>
2016-10-13 15:39:20 +0300

    Add services/api.getUnits, refs #7

--- app/services/api.spec.js
+++ app/services/api.spec.js
@@ -1,22 +1,22 @@
 import { expect } from 'chai';
 import simple from 'simple-mock';
 
-import { get } from './api';
+import api, { get } from './api';
 
 describe('services/api', () => {
+  let fetch;
+  let resolve;
+
+  beforeEach(() => {
+    fetch = simple.mock(window, 'fetch')
+      .returnWith(new Promise((res) => { resolve = res; }));
+  });
+
+  afterEach(() => {
+    simple.restore();
+  });
+
   describe('get', () => {
-    let fetch;
-    let resolve;
-
-    beforeEach(() => {
-      fetch = simple.mock(window, 'fetch')
-        .returnWith(new Promise((res) => { resolve = res; }));
-    });
-
-    afterEach(() => {
-      simple.restore();
-    });
-
     it('calls window.fetch', () => {
       get('test-url');
       expect(fetch.callCount).to.equal(1);
@@ -37,6 +37,30 @@ describe('services/api', () => {
       const expected = { some: 'data' };
       const response = { json: () => expected };
       const promise = get('test-url');
+      promise.then((actual) => {
+        expect(actual).to.equal(expected);
+        done();
+      });
+      resolve(response);
+    });
+  });
+
+  describe('getUnits', () => {
+    it('uses the correct URL', () => {
+      const expected =
+        'https://mock-api.fi/unit/?' +
+        'only=name%2Clocation%2Cstreet_address%2Caddress_zip%2Cmunicipality%2Cwww_url&' +
+        'page=1&page_size=1000&service=25304';
+      api.getUnits();
+      expect(fetch.callCount).to.equal(1);
+      const actual = fetch.lastCall.arg;
+      expect(actual).to.equal(expected);
+    });
+
+    it('returns results', (done) => {
+      const expected = { some: 'results' };
+      const response = { json: () => ({ results: expected }) };
+      const promise = api.getUnits();
       promise.then((actual) => {
         expect(actual).to.equal(expected);
         done();
