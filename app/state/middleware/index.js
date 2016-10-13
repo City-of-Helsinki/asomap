c5298f56ead5988c45985127a3b0fc0a72eb487d
Lauri Raivio <lauri.raivio@gmail.com>
2016-10-13 14:21:07 +0300

    Use redux-logger only on development
    Refs #1.

--- app/state/middleware/index.js
+++ app/state/middleware/index.js
@@ -1,11 +1,12 @@
 import { applyMiddleware } from 'redux';
 import { apiMiddleware } from 'redux-api-middleware';
-import createLogger from 'redux-logger';
 
 const isDevelopment = process.env.NODE_ENV !== 'production';
 const storeEnhancers = [applyMiddleware(apiMiddleware)];
 
 if (isDevelopment) {
+  const createLogger = require('redux-logger'); // eslint-disable-line global-require
+
   const loggerMiddleware = createLogger({
     collapsed: true,
     duration: true,
