f0ca812217f378547ce65121004ea52906603d5c
Oskari Hiltunen <felix.loether@gmail.com>
2016-10-20 15:55:45 +0300

    Remove example code, refs #24

--- app/state/reducers/index.js
+++ app/state/reducers/index.js
@@ -1,11 +1,9 @@
 import { combineReducers } from 'redux';
 
 import data from './dataReducer';
-import example from './exampleReducer';
 import filters from './filtersReducer';
 
 export default combineReducers({
   data,
-  example,
   filters,
 });
