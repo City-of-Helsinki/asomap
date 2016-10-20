ddb1a35f84cd604d53df50d7ee2ff04db1bb0bc0
Oskari Hiltunen <felix.loether@gmail.com>
2016-10-20 15:55:45 +0300

    Remove example code, refs #24

--- app/actions/uiActions.js
+++ app/actions/uiActions.js
@@ -3,9 +3,7 @@ import { createAction } from 'redux-actions';
 import types from './actionTypes';
 
 const changeCityFilter = createAction(types.CHANGE_CITY_FILTER);
-const changeMessage = createAction(types.CHANGE_MESSAGE);
 
 export default {
   changeCityFilter,
-  changeMessage,
 };
